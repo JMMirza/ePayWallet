37281a8a492e2757a2c1cb2fe957fe622b1beb3a
jmmirza <janmuhammadmirza@gmail.com>
2022-03-21 10:08:36 +0500

    changes

--- server.js
+++ server.js
@@ -3,25 +3,23 @@ const app = express();
 const connection = require("./utils/connection");
 const users = require("./router/userRouter");
 const partner = require("./router/partnerRouter");
-const admin = require("./router/adminRouter")
+const admin = require("./router/adminRouter");
 const bodyParser = require("body-parser");
-const Config = require("./config")
+const Config = require("./config");
 console.log("Project started");
 connection.getConnection();
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
+	bodyParser.urlencoded({
+		extended: false,
+	})
 );
 
 app.use("/api/user/", users);
 app.use("/api/user/", partner);
 app.use("/api/admin/", admin);
-// app.use("/api/admin/", admin);
-
 
 module.exports = app.listen(Config.server_port, () =>
-    console.log(`Project Started at port ${Config.server_port} environment  !!!`)
+	console.log(`Project Started at port ${Config.server_port} environment  !!!`)
 );
